0dc5ce54a969eff173e89e181c58244c2c864fce
boyad hollingshead <bhollingshead@rocktoptech.com>
2025-02-15 18:41:34 -0500

    fixed home screen responsiveness

--- src/components/tile.vue
+++ src/components/tile.vue
@@ -15,7 +15,9 @@ function navigate() {
   <div @click="navigate()"
       class="aspect-square bg-cover relative bg-blend-multiply bg-neutral-300 hover:bg-neutral-400 transition-colors cursor-pointer"
        :style="{'background-image': `url(${props.source })`}">
-    <span class="absolute text-light left-0 bottom-0 text-6xl font-bold w-1/2 uppercase">{{props.label}}</span>
+    <span
+        class="absolute text-light left-0 bottom-0 text-3xl sm:text-5xl md:text-4xl lg:text-[2.6rem] font-bold w-1/2 uppercase m-0.5">{{props
+        .label}}</span>
   </div>
 </template>
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,28 +1,39 @@
 <script setup lang="ts">
 import Tile from "@/components/tile.vue";
+
+const recentProjects = [
+  {
+    label: 'the lightning thief',
+    image: 'construct.jpeg',
+    link: 'the-lightning-thief'
+  },
+  {
+    label: 'hello dolly',
+    image: 'construct.jpeg',
+    link: 'hello-dolly'
+  },
+  {
+    label: 'snow white and the prince',
+    image: 'construct.jpeg',
+    link: 'snow-white-and-the-prince'
+  },
+  {
+    label: 'the lion king',
+    image: 'construct.jpeg',
+    link: 'the-lion-king'
+  }
+]
+
 </script>
 
 <template>
-  <section class="flex flex-col">
-    <div class="w-full h-24 px-64 py-2 flex items-end">
+  <section v-for="i of [0, 1]" v-bind="i" class="flex flex-col">
+    <div class="w-full h-24 px-16 md:px-32 lg:px-64 py-2 flex items-end">
       <span class="text-xl font-bold">RECENT PROJECTS</span>
     </div>
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-    <Tile label="HELLO DOLLY" source="construct.jpeg" link-to="hello-dolly"/>
-    <Tile label="HELLO DOLLY" source="construct.jpeg" />
-    <Tile class="hidden md:block" label="HELLO DOLLY" source="construct.jpeg" />
-    <Tile class="hidden lg:block" label="HELLO DOLLY" source="construct.jpeg" />
-    </div>
-  </section>
-  <section class="flex flex-col">
-    <div class="w-full h-24 px-64 py-2 flex items-end">
-      <span class="text-xl font-bold">RECENT PROJECTS</span>
-    </div>
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      <Tile label="HELLO DOLLY" source="construct.jpeg" />
-      <Tile label="HELLO DOLLY" source="construct.jpeg" />
-      <Tile class="hidden md:block" label="HELLO DOLLY" source="construct.jpeg" />
-      <Tile class="hidden lg:block" label="HELLO DOLLY" source="construct.jpeg" />
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+      <Tile v-for="(project, index) of recentProjects" v-bind="project" :label="project.label" :source="project.image"
+            :link-to="project.link" />
     </div>
   </section>
 </template>
