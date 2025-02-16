cf17403392b058a5f4a30ed1d0d0b34c8f583619
boyad hollingshead <bhollingshead@rocktoptech.com>
2025-02-16 00:10:02 -0500

    fixed footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,9 +5,8 @@ import LinkIcon from "@/components/link-icon.vue";
 <template>
   <div class="flex h-32 w-full items-center justify-between px-8 md:px-32 lg:px-64 bg-stone-950">
     <div class="flex flex-col underline underline-offset-2 nav-container">
-      <RouterLink to="/">Me</RouterLink>
-      <RouterLink to="/shows">Shows</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/">Shows</RouterLink>
+      <RouterLink to="/about">About Me</RouterLink>
       <RouterLink to="/contact">Contact</RouterLink>
     </div>
     <div class="flex flex-col text-right">
