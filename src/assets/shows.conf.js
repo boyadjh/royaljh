f2b2fc89b0ad2bda4646a3b2fa6aa26c986794cb
Royal Hollingshead <royalthepwnr@gmail.com>
2025-09-20 22:52:02 -0600

    changed show order

--- src/assets/shows.conf.js
+++ src/assets/shows.conf.js
@@ -1,11 +1,13 @@
-import { HELLO_DOLLY } from './hello-dolly.conf.js'
-import { SNOW_WHITE_AND_THE_PRINCE } from "./snow-white-and-the-prince.conf.js"
 import { ELEPHANTS_GRAVEYARD } from "./elephants_graveyard.conf.js"
+import { HELLO_DOLLY } from './hello-dolly.conf.js'
 import { LIGHTNING_THIEF } from "./lightning-thief.js"
+import { SNOW_WHITE_AND_THE_PRINCE } from "./snow-white-and-the-prince.conf.js"
 
 export const SHOWS = {
-  'hello-dolly': HELLO_DOLLY,
-  'snow-white-and-the-prince': SNOW_WHITE_AND_THE_PRINCE,
   'elephants-graveyard': ELEPHANTS_GRAVEYARD,
   'lightning-thief': LIGHTNING_THIEF,
+  'hello-dolly': HELLO_DOLLY,
+  'snow-white-and-the-prince': SNOW_WHITE_AND_THE_PRINCE,
+
+
 }
